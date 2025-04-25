48d729e4c148640cb499e2010399c3c587a1af5b
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-25 11:31:59 +0200

    SLight provider misconfiguration

--- lesgrandsvoisins/settings/base_local.py
+++ lesgrandsvoisins/settings/base_local.py
@@ -19,7 +19,7 @@ SOCIALACCOUNT_PROVIDERS = {
         # 'SOCIALACCOUNT_ONLY': True,
         "APPS": [
             {
-                "provider_id": "key.lesgrandsvoisins.com",
+                "provider_id": "key-lesgrandsvoisins-com",
                 "name": "key.lesgrandsvoisins.com",
                 "client_id": os.getenv("OPENID_NAME"),
                 "secret": os.getenv("OPENID_SECRET"),
